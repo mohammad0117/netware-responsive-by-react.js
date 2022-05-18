996930a18008a7b41c8feeaf8cc1b964aafc8828
mohammad molavi <mohamadmolavi98@gmail.com>
2022-05-18 16:43:39 +0430

    develop main-page component

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -10,15 +10,21 @@ class MainPage extends Component {
     render() { 
         return (
           <>
-            <Container fluid className="main-page mt-5 p-0" dir="rtl">
+            <Container fluid className="main-page mt-4" dir="rtl">
               <Col xs={12} className="main-page__desc">
-                <Row>
-                  <Col xs={6} className="main-page__desc__right-side pt-5 pe-0">
-                    <p>استودیو دیجیتال مارکتینگ نت ابزار</p>
-                    <h1 className="mt-2">
+                <Row className="flex-column-reverse flex-lg-row">
+                  <Col
+                    xs={12}
+                    lg={6}
+                    className="main-page__desc__right-side pt-5 pe-lg-5"
+                  >
+                    <p className="text-center text-lg-end pe-lg-4">
+                      استودیو دیجیتال مارکتینگ نت ابزار
+                    </p>
+                    <h1 className="mt-2 text-center text-lg-end pe-lg-4">
                       ارائـه خدمات هوشـمند برای رونـق کسـب و کـار شـما
                     </h1>
-                    <ul className="p-0 mt-4">
+                    <ul className="p-0 pe-lg-4 mt-4 mt-lg-5 text-center text-lg-end">
                       <li className="d-inline-block me-1 ms-4 position-relative">
                         برنامه ریزی
                       </li>
@@ -29,21 +35,21 @@ class MainPage extends Component {
                         توسعه
                       </li>
                     </ul>
-                    <div className="mt-5 d-flex justify-content-between">
+                    <div className=" d-flex justify-content-around px-3 px-lg-0 justify-content-lg-between">
                       <Link to="/" className="mt-5 text-decoration-none">
-                        <button className="shadow d-flex align-items-center pe-2 ps-4 py-3">
-                          <span className="px-3">G</span>
-                          <span className="mx-3">
+                        <button className="shadow d-flex flex-column align-items-center justify-content-center px-1 py-4 me-4 flex-lg-row py-lg-3 px-lg-3">
+                          <span className="px-3 p-lg-2 py-lg-0">G</span>
+                          <span className="mx-3 mt-3 mt-lg-0 mx-lg-1">
                             <b className="d-block">همکار رسمی گوگل</b> در ایران
                           </span>
                         </button>
                       </Link>
                       <Link to="/" className="mt-5 text-decoration-none">
-                        <button className="shadow d-flex align-items-center pe-2 ps-4 py-3">
-                          <span className="px-3">
+                        <button className="shadow d-flex flex-column align-items-center px-1 py-4 ms-4 flex-lg-row py-lg-3 px-lg-3 ms-lg-0">
+                          <span className="px-3 p-lg-2 py-lg-0">
                             <FontAwesomeIcon icon={faHeadset}></FontAwesomeIcon>
                           </span>
-                          <span className="mx-3">
+                          <span className="mx-3 mt-3 mt-lg-0 mx-lg-1">
                             <b className="d-block">مشاوره تخصصی</b> به صورت
                             رایگان
                           </span>
@@ -51,7 +57,11 @@ class MainPage extends Component {
                       </Link>
                     </div>
                   </Col>
-                  <Col xs={6} className="main-page__desc__left-side pt-5 pe-5">
+                  <Col
+                    xs={12}
+                    lg={6}
+                    className="main-page__desc__left-side pt-3 d-flex justify-content-center "
+                  >
                     <img src={softWareCompany} alt="software-company" />
                   </Col>
                 </Row>
