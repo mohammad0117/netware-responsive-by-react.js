c96edcd459d13f4f0653b66aa946b10c35a7a907
mohammad molavi <mohamadmolavi98@gmail.com>
2022-05-24 18:27:22 +0430

    develop main-page component

--- src/components/main-page.jsx
+++ src/components/main-page.jsx
@@ -19,20 +19,16 @@ class MainPage extends Component {
     return (
       <>
         <Container fluid className="main-page mt-4 p-0" dir="rtl">
-          <Col xs={12} className="main-page__desc position-relative px-3">
+          <Col xs={12} className="main-page__desc position-relative pt-5">
             <Row className="flex-column-reverse flex-lg-row">
-              <Col
-                xs={12}
-                lg={6}
-                className="main-page__desc__right-side pt-5 ps-0"
-              >
-                <p className="text-center text-lg-end pe-lg-4">
+              <Col xs={12} lg={6} className="main-page__desc__right-side pt-5">
+                <p className="text-center text-lg-end">
                   استودیو دیجیتال مارکتینگ نت ابزار
                 </p>
-                <h1 className="mt-2 text-center text-lg-end pe-lg-4">
+                <h1 className="mt-2 text-center text-lg-end">
                   ارائـه خدمات هوشـمند برای رونـق کسـب و کـار شـما
                 </h1>
-                <ul className="p-0 pe-lg-4 mt-4 mt-lg-5 text-center text-lg-end">
+                <ul className="p-0  mt-4 mt-lg-5 text-center text-lg-end">
                   <li className="d-inline-block me-1 ms-4 position-relative">
                     برنامه ریزی
                   </li>
@@ -43,9 +39,9 @@ class MainPage extends Component {
                     توسعه
                   </li>
                 </ul>
-                <div className=" d-flex justify-content-around px-3 px-lg-4 px-xxl-0 justify-content-lg-between">
+                <div className=" d-flex justify-content-around  px-xxl-0 justify-content-lg-between">
                   <Link to="/" className="mt-5 text-decoration-none">
-                    <button className="shadow d-flex flex-column align-items-center justify-content-center px-1 py-4 me-4 flex-lg-row py-lg-3 me-lg-0 px-lg-3 me-xxl-4">
+                    <button className="shadow d-flex flex-column align-items-center justify-content-center px-1 py-4 me-4 flex-lg-row py-lg-3 me-lg-0 px-lg-3">
                       <span className="px-3 p-lg-2 py-lg-0 px-xxl-3">G</span>
                       <span className="mx-3 mt-3 mt-lg-0 mx-lg-1 me-xxl-2 ms-xxl-4">
                         <b className="d-block">همکار رسمی گوگل</b> در ایران
@@ -53,7 +49,7 @@ class MainPage extends Component {
                     </button>
                   </Link>
                   <Link to="/" className="mt-5 text-decoration-none">
-                    <button className="shadow d-flex flex-column align-items-center px-1 py-4 ms-4 flex-lg-row py-lg-3 px-lg-3 ms-lg-0">
+                    <button className="shadow d-flex flex-column align-items-center px-1 py-4 ms-4 flex-lg-row py-lg-3 px-lg-3 ms-xxl-1">
                       <span className="px-3 p-lg-2 py-lg-0 px-xxl-3">
                         <FontAwesomeIcon icon={faHeadset}></FontAwesomeIcon>
                       </span>
@@ -67,9 +63,15 @@ class MainPage extends Component {
               <Col
                 xs={12}
                 lg={6}
-                className="main-page__desc__left-side pt-3 d-flex justify-content-center "
+                className="main-page__desc__left-side pt-3 d-flex flex-row-reverse"
               >
-                <img src={softWareCompany} alt="software-company" />
+                <div>
+                  <img
+                    className="img-fluid"
+                    src={softWareCompany}
+                    alt="software-company"
+                  />
+                </div>
               </Col>
             </Row>
             <button
@@ -83,14 +85,14 @@ class MainPage extends Component {
               ></FontAwesomeIcon>
             </button>
           </Col>
-          <Col xs={12} className="main-page__items px-lg-3">
+          <Col xs={12} className="main-page__items bg-white">
             <div className="main-page__items__wrapper grid pt-3 py-lg-5">
               <Col lg={4} className="d-none d-lg-block">
                 <div className="main-page__items__wrapper__card picture px-0">
                   <img src={softWareDeveloper} alt="" />
                 </div>
               </Col>
-              <Col lg={4}>
+              <Col lg={4} className="d-flex justify-content-center">
                 <div className="main-page__items__wrapper__card second d-flex flex-column justify-content-center mx-3 mx-lg-0  pt-lg-5">
                   <FontAwesomeIcon
                     className="fa-time-line mt-5"
@@ -105,7 +107,7 @@ class MainPage extends Component {
                   </p>
                 </div>
               </Col>
-              <Col lg={4}>
+              <Col lg={4} className="d-flex flex-row-reverse">
                 <div className="main-page__items__wrapper__card third d-flex flex-column justify-content-center mx-3 mx-lg-0 pt-lg-5">
                   <FontAwesomeIcon
                     className="fa-time-line mt-5"
@@ -120,7 +122,7 @@ class MainPage extends Component {
                 </div>
               </Col>
               <Col lg={4}>
-                <div className="main-page__items__wrapper__card fourth d-flex flex-column justify-content-center mx-3 mx-lg-0 pt-4 pt-lg-5 mt-lg-5">
+                <div className="main-page__items__wrapper__card fourth d-flex flex-column justify-content-center mx-3 mx-lg-0 pt-4 pt-lg-5 mt-lg-4 mt-xl-5">
                   <FontAwesomeIcon
                     className="fa-time-line mt-5"
                     icon={faFileAlt}
@@ -134,8 +136,8 @@ class MainPage extends Component {
                   </p>
                 </div>
               </Col>
-              <Col lg={4}>
-                <div className="main-page__items__wrapper__card fifth d-flex flex-column justify-content-center mx-3 mx-lg-0 mt-lg-5 pt-lg-4">
+              <Col lg={4} className="d-flex justify-content-center">
+                <div className="main-page__items__wrapper__card fifth d-flex flex-column justify-content-center mx-3 mx-lg-0 mt-lg-4 pt-lg-4 mt-xl-5">
                   <FontAwesomeIcon
                     className="fa-time-line mt-5"
                     icon={faMagnifyingGlassPlus}
@@ -149,8 +151,8 @@ class MainPage extends Component {
                   </p>
                 </div>
               </Col>
-              <Col lg={4} className="d-none d-lg-block">
-                <div className="main-page__items__wrapper__card picture  d-none d-lg-block px-0 mt-lg-5">
+              <Col lg={4} className="d-flex flex-row-reverse">
+                <div className="main-page__items__wrapper__card picture  d-none d-lg-block px-0 mt-lg-4 mt-xl-5">
                   <img src={woman} alt="" />
                 </div>
               </Col>
